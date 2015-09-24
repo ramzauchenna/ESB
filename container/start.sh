c415ca6a4349873f9830d50aa90268692b6b5d8f
uchenna <ramzauchenna@gmail.com>
2015-09-24 21:11:11 +0100

    exposed on a singlee pot

--- container/start.sh
+++ container/start.sh
@@ -2,6 +2,6 @@
 export NODE_PATH=/ESB/
 export SWARM_NODE_TYPE='demo'
 export SWARM_PATH=/ESB/
-http-server /ESB/public/ -p 8080&
+http-server /ESB/public/ -p 80&
 nohup redis-server &
 node /ESB/Adapters/Luancher.js
