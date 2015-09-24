cc8bb088589b48d1a417e5fd198dc98fb7437cd0
uchenna <ramzauchenna@gmail.com>
2015-09-24 10:26:21 +0100

    added docker api

--- container/start.sh
+++ container/start.sh
@@ -3,4 +3,4 @@ export NODE_PATH=/opt/aws/opsworks/current/site-cookbooks/
 export SWARM_NODE_TYPE='demo'
 export SWARM_PATH=/opt/aws/opsworks/current/site-cookbooks/
 nohup redis-server &
-node /opt/aws/opsworks/current/site-cookbooks/Adapters/Launcher.j
+node /opt/aws/opsworks/current/site-cookbooks/Adapters/Launcher.js
