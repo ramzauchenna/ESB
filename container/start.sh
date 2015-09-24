307338a2884d6cf2c67c9486127dfc4803823ef5
uchenna <ramzauchenna@gmail.com>
2015-09-24 17:22:11 +0100

    sett docker path

--- container/start.sh
+++ container/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export NODE_PATH=/home/c2g/ESB/
+export NODE_PATH=/ESB/
 export SWARM_NODE_TYPE='demo'
-export SWARM_PATH=/home/c2g/ESB/
+export SWARM_PATH=/ESB/
 nohup redis-server &
-node /home/c2g/ESB/Adapters/Launcher.js
+node /ESB/Adapters/Launcher.js
